bed9f865a7d2405d3d45efdef77421d2e84d78cd
unknown <javi.mariscal13@gmail.com>
2022-02-12 18:50:24 -0500

    Added help from homework guide

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,28 @@
-console.log("Ello Govner")
+
+var currentQuestion = 0;
+var time = questions.length * 15;
+var timer;
+
+var startButton = document.querySelector("#start");
+var questionsElement = document.querySelector("#questions");
+var timerElement = document.querySelector("#time");
+var questionChoices = document.querySelector("#choices");
+
+// Function for Kicking off the quiz
+
+function startQuiz() {
+    var homeScreen = document.querySelector("#start-screen");
+    startScreen.setAttribute("class", "hide");
+
+    questionsElement.removeAttribute("class");
+
+    getCurrentQuestion();
+}
+
+function getCurrentQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleElement = document.querySelector("#question-title");
+    titleElement.textContent = currentQuestion.title;
+
+    questionChoices.textContent = "";
+}
